b836e65589ee29462d82a837715914835101cc54
[abe-tk] <[abe.tk6@gmail.com]>
2024-02-10 17:45:23 +0900

    cloudFunctionsの導入

---
+++ functions/functions/src/index.ts
@@ -0,0 +1,10 @@
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
